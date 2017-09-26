966e1bec6ef5ccb8252c8f00d0debec9d37ff6ac
Iuzziel <Iuzziel@users.noreply.github.com>
2017-09-26 15:15:14 +0200

    Create accueil.php

---
+++ vues/accueil.php
@@ -0,0 +1,9 @@
+<section>
+				<h2>Accueil</h2>
+    <ul>
+        <li><a href="index.php?choix=abonnes1">Liste</a> des contacts 
+        (ou une autre <a href="index.php?choix=abonnes2">présentation</a> des contacts)</li>
+        <li>Saisir un <a href="index.php?choix=ajouter">nouveau contact</a></li>
+        <li><a href="index.php?choix=gestionAbonne">Modifier/Supprimer</a> un contact</li>
+    </ul>
+</section>
